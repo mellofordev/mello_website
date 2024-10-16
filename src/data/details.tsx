7d2b2224cb241b2565cc6fc585873bf15f5887cc
mellofordev <sreedharks2003@gmail.com>
2024-10-16 20:48:55 +0530

    refactor(data): update company description and summary

--- src/data/details.tsx
+++ src/data/details.tsx
@@ -9,7 +9,7 @@ export const DATA = {
   description:
     "shipping cool products.",
   summary:
-    "We're the company behind sapient,a productivity tool and os",
+    "we're bunch of students trying to build cool products for the community, we started back in 2016 building small websites, now we are on the mission to create products with generative ai",
   avatarUrl: "/mello.jpeg",
   
   navbar: [
